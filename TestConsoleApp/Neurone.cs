22637d00e18c064408714e53fcb7746ca393e67a
Filip Dryja <f.dryja1@gmail.com>
2019-12-05 13:55:37 +0100

    Dodaj pliki projektów.

---
+++ TestConsoleApp/Neurone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestConsoleApp
+{
+    public class Neurone
+    {
+        public Neurone()
+        {
+        }
+
+        public Neurone(List<float> inputs, List<float> weights, float bias, String function)
+        {
+            float result = 0;
+            for (int i = 0; i <= inputs.Count; i++)
+            {
+                result += inputs[i] * weights[i];
+            }
+            result += bias;
+
+
+            switch (function)
+            {
+                case "f1":
+                    //f1 return();
+                    break;
+                case "f2":
+                    //f2 return();
+                    break;
+                case "f3":
+                    //f3 return();
+                    break;
+            }
+        }
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TestConsoleApp;
+using System.Collections.Generic;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ListaList/TablicaTablic NetStucture =
+
+            List<float> inputs;
+            List<float> weights;
+            float bias;
+
+            string[] functionNames = { "f1", "f2", "f3" };
+
+            //Stworzenie neuronu
+
+            for (i)
+            {
+                for (j)
+                {
+                    //Wynik[i][j] = Neurone(inputs, weights, bias, functionNames[ NetStructure[i][j] ])
+                }
+            }
+
+        }
+    }
+}
