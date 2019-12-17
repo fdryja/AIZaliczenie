427211fa06fe088ceb47a8f2129b064e710216f4
Filip Dryja <f.dryja1@gmail.com>
2019-12-17 19:00:04 +0100

    Zmiana przypisywania losowych wag z for na foreach.

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -12,13 +12,15 @@ namespace TestConsoleApp
             List<List<float>> xses = new List<List<float>>();
             //List<List<float>> weights = new List<List<float>>();
 
-            List<float> weight = new List<float>();
+            List<List<float>> weight = new List<List<float>>();
 
             Random rnd = new Random();
-            for (int r = 0; r < netStructure.Count; r++)
+
+            foreach (var i in weight)
             {
-                weight.Add(rnd.Next(1, 11));
+                i.Add(rnd.Next(1, 11));
             }
+
             float bias = 0;
 
             string[] functionNames = new string[] {
@@ -35,7 +37,7 @@ namespace TestConsoleApp
             {
                 for (int j = 0; i < netStructure[i].Count; j++)
                 {
-                    xses[i + 1].Add(neurone.NeuroneFunction(xses[i], weight, bias, functionNames[netStructure[i][j]]));
+                    xses[i + 1].Add(neurone.NeuroneFunction(xses[i], weight[i], bias, functionNames[netStructure[i][j]]));
                 }
             }
 
