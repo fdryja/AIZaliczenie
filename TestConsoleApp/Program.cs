a4aa2f9a6aaf08c97db462ff1f94dc165787007a
Remo <remoplyt@gmail.com>
2019-12-18 19:36:42 +0100

    Preparation to tests current version

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -10,16 +10,37 @@ namespace TestConsoleApp
         {
             List<List<int>> netStructure = new List<List<int>>();
             List<List<float>> xses = new List<List<float>>();
-            //List<List<float>> weights = new List<List<float>>();
-
             List<List<float>> weight = new List<List<float>>();
 
+            List<int> temp = new List<int>();
+            List<float> temp2 = new List<float>();
+            List<float> temp3 = new List<float>();
+
             Random rnd = new Random();
-            for (int r = 0; r < netStructure.Count; r++)
+
+            float bias = 1;
+
+            for (int i = 0; i < netStructure.Count; i++)
             {
-                weight[r].Add(rnd.Next(1, 11));
+                for (int j = 0; i < netStructure[i].Count; j++)
+                {
+                    temp.Add(rnd.Next(1, 3));
+                }
+                netStructure.Add(temp);
+                temp.Clear();
             }
-            float bias = 0;
+
+            for (int i = 0; i < netStructure.Count; i++)
+            {
+                for (int j = 0; i < netStructure[i].Count; j++)
+                {
+                    temp2.Add(rnd.Next(1, 11));
+                }
+                weight.Add(temp2);
+                temp2.Clear();
+            }
+
+
 
             string[] functionNames = new string[] {
                 "Liniowa",
@@ -35,11 +56,17 @@ namespace TestConsoleApp
             {
                 for (int j = 0; i < netStructure[i].Count; j++)
                 {
-                    xses[i + 1].Add(neurone.NeuroneFunction(xses[i], weight[i], bias, functionNames[netStructure[i][j]]));
+                    temp3.Add(neurone.NeuroneFunction(xses[i], weight[i], bias, functionNames[netStructure[i][j]]));
                 }
+                xses.Add(temp3);
+                temp3.Clear();
             }
-            
-            //Console.WriteLine(xses[netStructure.Count - 1]);
+
+
+           // for (int n = 0; n < netStructure[netStructure.Count].Count; n++) 
+          //  {
+          //      Console.WriteLine(xses[netStructure.Count][n]);
+          //  }
 
         }
     }
