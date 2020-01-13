f34ff6e66aa7f4285a7a9d79e166a4e654390b9f
Filip <f.dryja1@gmail.com>
2020-01-13 15:02:32 +0100

    w trakcie pracy

--- TestConsoleApp/NeuralNet.cs
+++ TestConsoleApp/NeuralNet.cs
@@ -382,5 +382,153 @@ namespace TestConsoleApp
             writer.Close();
         }
 
+
+        public int[] ReadNetStructure()
+        {
+
+            return null;
+        }
+
+
+
+        public static void ReadToLearn()
+        {
+            int linesCount = 0;
+
+            String netStructureLine, xsesLine, expectedString;
+            expectedString = "";
+            netStructureLine = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"Data\data.txt"))
+                {
+                    //policzenie linii w tekście
+
+                    linesCount = File.ReadAllLines(@"Data\data.txt").Length;
+
+                    int ileWarstw = 0;
+                    String useless = "";
+
+                    for (int i = 0; i < linesCount; i++)
+                    {
+                        useless = sr.ReadLine();
+                        if (useless.Equals("/") == false) { ileWarstw++; } else { break; }
+
+                    }
+                    sr.DiscardBufferedData();
+
+                    StreamReader sr1 = new StreamReader(@"Data\data.txt");
+
+
+                    int[][] netStructure = new int[ileWarstw][];
+
+                    //XSES
+                    float [][] learnXses = new float[linesCount - ileWarstw - 1][];
+
+                    float[]expected = new float[linesCount - 1];
+
+
+
+                    List<float> listLearnXses = new List<float>();
+                    List<int> listNetStructure = new List<int>();
+
+                    for (int i = 0; i < ileWarstw; i++)
+                    {
+                        netStructureLine = sr1.ReadLine();
+
+                        netStructureLine.Trim();
+
+                        netStructureLine += ' ';
+                        for (int j = 0; j < netStructureLine.Length; j++)
+                        {
+                            if (netStructureLine[j] != ' ')
+                            {
+                                expectedString += netStructureLine[j];
+                            }
+                            else if (netStructureLine[j] == ' ')
+                            {
+                                if (expectedString == "/") { break; }
+                                listNetStructure.Add(int.Parse(expectedString));
+                                expectedString = "";
+                            }
+                        }
+                        netStructure[i] = new int[listNetStructure.Count];
+
+
+                        for (int l = 0; l < netStructure[i].Length; l++)
+                        {
+                            netStructure[i][l] = listNetStructure[l];
+                        }
+                        listNetStructure.Clear();
+                    }
+                    Console.WriteLine(linesCount - ileWarstw);
+
+                    for (int i = 0; i < linesCount - ileWarstw - 1; i++)
+                    {
+                        xsesLine = sr1.ReadLine();
+                        Console.WriteLine(xsesLine);
+                        xsesLine.Trim();
+                        xsesLine += ' ';
+                        for (int j = 0; j < xsesLine.Length; j++)
+                        {
+                            if (xsesLine[j] != 'e' && xsesLine[j] != ' ')
+                            {
+                                expectedString += xsesLine[j];
+                            }
+                            else if (xsesLine[j] == 'e')
+                            {
+                                expected[i] = float.Parse(expectedString, System.Globalization.CultureInfo.InvariantCulture);
+                                expectedString = "";
+                            }
+                            else if (xsesLine[j] == ' ')
+                            {
+                                if (expectedString != "/")
+                                {
+                                    listLearnXses.Add(float.Parse(expectedString, System.Globalization.CultureInfo.InvariantCulture));
+                                }
+                                expectedString = "";
+                            }
+                        }
+                        learnXses[i] = new float[listLearnXses.Count];
+                        Console.WriteLine(listLearnXses.Count);
+                        for (int l = 0; l < listLearnXses.Count; l++)
+                        {
+                            learnXses[i][l] = listLearnXses[l];
+                        }
+                        listLearnXses.Clear();
+                    }
+
+
+
+
+                    for (int i = 0; i < learnXses.Length; i++)
+                    {
+                        for (int j = 0; j < learnXses[i].Length; j++)
+                        {
+                            Console.Write(learnXses[i][j] + ", ");
+                        }
+                        Console.WriteLine("");
+                    }
+                    Console.WriteLine("");
+                    for (int i = 0; i < netStructure.Length; i++)
+                    {
+                        for (int j = 0; j < netStructure[i].Length; j++)
+                        {
+                            Console.Write(netStructure[i][j] + ", ");
+                        }
+                        Console.WriteLine("");
+                    }
+
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Nie można odczytać");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
     }
 }
--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -14,120 +14,153 @@ namespace TestConsoleApp
             Random rand = new Random();
 
             int wyborMetody;
-
+            
 
             Console.WriteLine("Wybierz metodę wprowadzania danych:\n1 - ręcznie\n2 - odczyt z pliku");
             wyborMetody = Convert.ToInt16(Console.ReadLine());
 
-            if(wyborMetody == 1)
+            if (wyborMetody == 1)
             {
-                Console.WriteLine("Podaj liczbę warstw");
-                int layersCount = Convert.ToInt32(Console.ReadLine());
 
-                int[] layers = new int[layersCount];
-                string[] layerActivations = new string[layersCount];
-                float[] expected = new float[] { 1000 };
-
-                for (int i = 1; i < layers.Length; i++)
+                int wyborTypu;
+                Console.WriteLine("Wybierz typ działania programu:\n1 - jednokierunkowe\n2 - ze sprzężeniem zwrotnym");
+                wyborTypu = Convert.ToInt16(Console.ReadLine());
+                if (wyborTypu == 1)
                 {
 
-                    if (i == layers.Length - 1)
-                    {
-                        layers[i] = expected.Length;
-                        break;
-                    }
-
-                    int neurones;
-                    Console.WriteLine("Podaj liczbę neuronów dla warstwy " + (i + 1));
-                    neurones = Convert.ToInt32(Console.ReadLine());
-                    layers[i] = neurones;
                 }
-
-                //string[] layerActivations = new string[] { "sigmoid", "tanh", "sigmoid" };
-
-
-
-                for (int i = 0; i < layersCount; i++)
+                else if (wyborTypu == 2)
                 {
-                OnceAgain:
-                    Console.WriteLine("Podaj funckję aktywacji dla warstwy " + (i + 1) + "\n1 - sigmoidalna\n2 - tangens hiperboliczny\n3 - ReLU\n4 - Leaky ReLU");
-                    int wybor = Convert.ToInt32(Console.ReadLine());
-                    if (wybor == 1)
-                    {
-                        layerActivations[i] = "sigmoid";
-                        Console.WriteLine("Funkcja aktywacji dla warstwy " + (i + 1) + " ustawiona na sigmoidalną!");
 
-                    }
-                    else if (wybor == 2)
-                    {
-                        layerActivations[i] = "tanh";
-                        Console.WriteLine("Funkcja aktywacji dla warstwy " + (i + 1) + " ustawiona na tangens hiperboliczny!");
-                    }
-                    else if (wybor == 3)
-                    {
-                        layerActivations[i] = "relu";
-                        Console.WriteLine("Funkcja aktywacji dla warstwy " + (i + 1) + " ustawiona na ReLU!");
+                    //float[] tab = new float[4];
+                    float[] expected = new float[] { 1000 };
+                    //float[] tab = new float[] { 80, 186, 3 };
 
-                    }
-                    else if (wybor == 4)
+                    Console.WriteLine("Podaj liczbę wejść");
+                    int inputsCount = Convert.ToInt32(Console.ReadLine());
+
+                    float[] tab = new float[inputsCount];
+
+                    for (int i = 0; i < tab.Length; i++)
                     {
-                        layerActivations[i] = "leakyrelu";
-                        Console.WriteLine("Funkcja aktywacji dla warstwy " + (i + 1) + " ustawiona na Leaky ReLU!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("nie ma takiego numeru");
-                        goto OnceAgain;
+                        Console.WriteLine("Podaj wartość dla wejścia numer " + (i + 1));
+                        float input = (float)Convert.ToDouble(Console.ReadLine());
                     }
 
+
+                   
+
+                    Console.WriteLine("Podaj liczbę warstw");
+                    int layersCount = Convert.ToInt32(Console.ReadLine());
+
+                    int[] layers = new int[layersCount];
+                    string[] layerActivations = new string[layersCount];
+                    layers[0] = tab.Length;
+
+                    for (int i = 1; i < layers.Length; i++)
+                    {
+
+                        if (i == layers.Length - 1)
+                        {
+                            layers[i] = expected.Length;
+                            break;
+                        }
+
+                        int neurones;
+                        Console.WriteLine("Podaj liczbę neuronów dla warstwy " + (i + 1));
+                        neurones = Convert.ToInt32(Console.ReadLine());
+                        layers[i] = neurones;
+                    }
+
+                    //string[] layerActivations = new string[] { "sigmoid", "tanh", "sigmoid" };
+
+
+
+                    for (int i = 0; i < layersCount; i++)
+                    {
+                    OnceAgain:
+                        Console.WriteLine("Podaj funckję aktywacji dla warstwy " + (i + 1) + "\n1 - sigmoidalna\n2 - tangens hiperboliczny\n3 - ReLU\n4 - Leaky ReLU");
+                        int wybor = Convert.ToInt32(Console.ReadLine());
+                        if (wybor == 1)
+                        {
+                            layerActivations[i] = "sigmoid";
+                            Console.WriteLine("Funkcja aktywacji dla warstwy " + (i + 1) + " ustawiona na sigmoidalną!");
+
+                        }
+                        else if (wybor == 2)
+                        {
+                            layerActivations[i] = "tanh";
+                            Console.WriteLine("Funkcja aktywacji dla warstwy " + (i + 1) + " ustawiona na tangens hiperboliczny!");
+                        }
+                        else if (wybor == 3)
+                        {
+                            layerActivations[i] = "relu";
+                            Console.WriteLine("Funkcja aktywacji dla warstwy " + (i + 1) + " ustawiona na ReLU!");
+
+                        }
+                        else if (wybor == 4)
+                        {
+                            layerActivations[i] = "leakyrelu";
+                            Console.WriteLine("Funkcja aktywacji dla warstwy " + (i + 1) + " ustawiona na Leaky ReLU!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("nie ma takiego numeru");
+                            goto OnceAgain;
+                        }
+
+                    }
+
+
+
+
+
+
+
+                    for (int i = 0; i < layers.Length; i++)
+                    {
+                        Console.WriteLine("Liczba neuronów, wastwa " + (i + 1) + "||" + layers[i]);
+                    }
+
+                    for (int i = 0; i < layers.Length; i++)
+                    {
+                        Console.WriteLine("Nazwa funckcji, wastwa " + (i + 1) + "||" + layerActivations[i]);
+                    }
+
+
+                    
+
+                    //for (int i = 0; i < tab.Length; i++)
+                    //{
+                    //    tab[i] =(float) rand.NextDouble() * rand.Next(1, 80);
+                    //}
+
+                    NeuralNet neuralNet = new NeuralNet(layers, layerActivations);
+                    neuralNet.Load(Path.Combine(Environment.CurrentDirectory, @"Data\text.txt"));
+                    for (int i = 0; i < neuralNet.FeedForward(tab).Length; i++)
+                    {
+                        Console.WriteLine("Element zwróconej tablicy numer " + (i + 1) + " " + neuralNet.FeedForward(tab)[i]);
+                    }
+
+
+                    for (int i = 0; i < neuralNet.FeedForward(tab).Length; i++)
+                    {
+                        neuralNet.BackPropagate(tab, expected);
+                        Console.WriteLine("Element zwróconej tablicy numer " + (i + 1) + " " + neuralNet.FeedForward(tab)[i]);
+                    }
+
+
+                    neuralNet.Save(Path.Combine(Environment.CurrentDirectory, @"Data\weightsAndBiases.txt"));
                 }
-
-
-
-
-
-
-
-                for (int i = 0; i < layers.Length; i++)
+                else
                 {
-                    Console.WriteLine("Liczba neuronów, wastwa " + (i + 1) + "||" + layers[i]);
+                    Console.WriteLine("Nie ma takiego numeru");
                 }
-
-                for (int i = 0; i < layers.Length; i++)
-                {
-                    Console.WriteLine("Nazwa funckcji, wastwa " + (i + 1) + "||" + layerActivations[i]);
-                }
-
-
-                //float[] tab = new float[4];
-
-                float[] tab = new float[] { 80, 186, 3 };
-                layers[0] = tab.Length;
-
-                //for (int i = 0; i < tab.Length; i++)
-                //{
-                //    tab[i] =(float) rand.NextDouble() * rand.Next(1, 80);
-                //}
-
-                NeuralNet neuralNet = new NeuralNet(layers, layerActivations);
-                neuralNet.Load(Path.Combine(Environment.CurrentDirectory, @"Data\text.txt"));
-                for (int i = 0; i < neuralNet.FeedForward(tab).Length; i++)
-                {
-                    Console.WriteLine("Element zwróconej tablicy numer " + (i + 1) + " " + neuralNet.FeedForward(tab)[i]);
-                }
-
-
-                for (int i = 0; i < neuralNet.FeedForward(tab).Length; i++)
-                {
-                    neuralNet.BackPropagate(tab, expected);
-                    Console.WriteLine("Element zwróconej tablicy numer " + (i + 1) + " " + neuralNet.FeedForward(tab)[i]);
-                }
-
-
-                neuralNet.Save(Path.Combine(Environment.CurrentDirectory, @"Data\weightsAndBiases.txt"));
+                
             }else if(wyborMetody == 2)
             {
-
+                NeuralNet.ReadToLearn();
+                
             }
             else
             {
